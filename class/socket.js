c4f589493248c322382fc4d47b559ed5a7be7446
Safanar <satan251008@gmail.com>
2017-06-14 16:19:39 +0800

    Add files via upload

--- class/socket.js
+++ class/socket.js
@@ -17,7 +17,21 @@ class SocketIO
 							socket.emit('bikes',func.dberror());
 						}
 						else{
-							socket.emit('bikes',func.result(data,1));
+							//socket.emit('bikes',func.result(data,1));
+							var ourbikes = [];
+							for(var i=0;i<data.length;i++){
+								var obj = new Object;
+								obj.id = data[i]._id;
+								if(data[i].data.charAt(8) == 0)obj.state = 0;
+								else obj.state = 1;
+								obj.battery = data[i].data.charAt(4);
+								obj.location = data[i].location;
+								//obj.kid = null;
+								d = new Date();
+								obj.time = new Date(new Date(data[i].time) - (d.getTimezoneOffset() * 60000));
+								ourbikes[i] = obj;
+							};
+							socket.emit('bikes',func.result(ourbikes,1));
 						}
 					});
 				}

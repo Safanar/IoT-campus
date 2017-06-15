7b58fc9149f5d45fd93c375c7a87f18f5861fbdc
Safanar <satan251008@gmail.com>
2017-06-15 13:28:58 +0800

    Add files via upload

--- class/socket.js
+++ class/socket.js
@@ -27,13 +27,13 @@ class SocketIO
 								obj.battery = data[i].data.charAt(8)%2;
 								obj.location = data[i].location;
 								//obj.kid = null;
+								var d = new Date();
 								obj.time = data[i].time;
 								obj.lasttime = new Date(0).getTime();
-								if(bool == true && ourbikes[i].state != obj.state){
-									obj.battery = 1;
-									obj.lasttime = new Date().getTime();
-									/*that.mongoDataBase.updateBike(obj.id,obj.lasttime,function(err,data) {
-									});*/
+								if(bool == true){
+									obj.lasttime = d.getTime();
+									that.mongoDataBase.updateBike(obj.id,obj.lasttime,function(err,data) {
+									});
 								}
 								ourbikes[i] = obj;
 							};
